21ce6939c881a95ea3b43148c70e0c7da3fc84ec
Lipski.Mikalai <lipski.mikalai@gmail.com>
2021-05-23 11:17:08 +0300

    changed the order of class instance definition

--- task_2/task_2.py
+++ task_2/task_2.py
@@ -1,6 +1,53 @@
+import re
+
+
 class Version:
     def __init__(self, version):
-        self.version = tuple(x for x in version.split('.'))
+        """
+        The versions are reduced to a format of five numeric values separated by a dot.
+        1.2.0.0.0
+        The class returns a tuple.
+
+        :param version: str
+        :return tuple: ('*','*','*','*','*')
+        """
+        lst = version.split('.')
+        version_lst = []
+
+        for ind in lst:
+
+            verbal_version = re.findall('[a-z]{1,5}', ind)
+
+            if verbal_version != []:
+                try:
+                    temp_val = verbal_version[0]
+                    if temp_val == 'a' or temp_val == 'alpha':
+                        r = 0
+                    elif temp_val == 'b' or temp_val == 'beta':
+                        r = 1
+                    elif temp_val == 'rc':
+                        r = 2
+                    elif temp_val == 'r':
+                        r = 3
+                    else:
+                        raise ValueError
+                except ValueError:
+                    print('Undefined version')
+                    break
+
+                digital_version = re.findall('\d{1,5}', ind)
+                if digital_version != []:
+                    version_lst.append(digital_version[0])
+
+                version_lst.append(r)
+
+            else:
+                version_lst.append(ind)
+
+        while len(version_lst) < 5:
+            version_lst.append('0')
+
+        self.version = tuple(version_lst)
 
     def __le__(self, other):
         return self.version <= other.version
@@ -30,17 +77,6 @@ def main():
         ("1.0.1b", "1.0.10-alpha.beta"),
         ("1.0.0-rc.1", "1.0.0"),
     ]
-
-    for version_1, version_2 in to_test:
-        assert Version(version_1) < Version(version_2), "le failed"
-        assert Version(version_2) > Version(version_1), "ge failed"
-        assert Version(version_2) != Version(version_1), "neq failed"
-
-
-print(Version('1.3.42') == Version('42.3.1'))  # False+
-print(Version('0.3.0b') < Version('1.2.42'))  # True+
-print(Version('1.3.0') > Version('0.3.0'))  # True+
-print(Version('1.1.3') < Version('2.2.3'))  # True+
 
 
 if __name__ == "__main__":
